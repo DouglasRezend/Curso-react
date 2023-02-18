8c954b4dac465afc22bc780879c280bbce620c90
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-02-18 20:50:18 -0300

    funçoes no evento

--- 2-FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 2-FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -8,6 +8,15 @@ const Events = () => {
             <div>
                 <button onClick={handleMyEvent}>Clique aqui!</button>
             </div>
+            <div>
+                <button onClick={() => console.log("clicou!")}>Clique aqui também!</button>
+                <button onClick={() => {
+                    console.log("isso não deveria existir =)")
+                }}
+                >
+                    Clique aqui, por favor.
+                    </button>
+            </div>
         </div>
     )
 } ;
