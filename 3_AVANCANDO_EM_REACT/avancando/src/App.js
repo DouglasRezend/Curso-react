deb92e56cac3375d3c889731597dfd2723bcd14e
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-02-21 18:44:11 -0300

    desafio

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -9,6 +9,10 @@ import { useState } from 'react';
 import CarDetails from './components/CarDetails';
 import Fragment from './components/Fragment';
 import Container from './components/Container';
+import ExecuteFunction from './components/ExecuteFunction';
+import Message from './components/Message';
+import ChangeMessageState from './components/ChangeMessageState';
+import UserDetails from './components/UserDetails';
 
 function App() {
   //const name ="Pedrin"
@@ -18,7 +22,24 @@ function App() {
     {id: 1, brand: "Ferrari",color:"amarela", newCar: true, km:0},
     {id: 2, brand: "Kia",color:"verde", newCar: false, km:12323},
     {id: 3, brand: "Renault",color:"azul", newCar: false, km:123123}
+  ];
+  function showMessage() {
+    console.log("Evento de componente pai!")
+  }
+
+  const [message , setMessage] = useState ("");
+
+  const handleMessage = (msg) => {
+    setMessage(msg);
+  };
+
+  const users = [
+    {id: 1, name:"Douglas", job:"Programador", age: 26},
+    {id: 2, name:"Adriana", job:"Designer de interiores", age: 29},
+    {id: 3, name:"Neia", job:"cozinheira", age: 52},
+    {id:4, name:"Pedro",job:"Estudante", age:17}
   ]
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -42,7 +63,9 @@ function App() {
       <CarDetails brand ="Fiat" km={1000} color="Preto" newCar ={false} />
       {/* loop em array de objetos*/}
       {cars.map((car) =>(
-        <CarDetails  brand ={car.brand} color={car.color} km={car.km} newCar={car.newCar}/>
+        <CarDetails
+        key={car.id}
+          brand ={car.brand} color={car.color} km={car.km} newCar={car.newCar}/>
       ))}
       {/*fragment */}
       <Fragment propFragment ="teste"/>
@@ -53,6 +76,22 @@ function App() {
       <Container myValue="testing 2">
         <h4>Testando o container</h4>
       </Container>
+      {/*executar funçao*/ }
+      <ExecuteFunction myFunction={showMessage}/>
+      {/* state lift */}
+      <Message msg={message} />
+      <ChangeMessageState handleMessage ={handleMessage}/>
+      {/* Desafio */}
+      {users.map((user) =>(
+        <UserDetails 
+        key={user.id}
+        name={user.name}
+        job={user.job}
+        age={user.age}
+        />
+
+      ))}
+
     </div>
   );
 }
