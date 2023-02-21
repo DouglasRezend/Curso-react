93f5b3a4bc2ea1337e602757c4a6b39e10ccb3bd
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-02-21 17:51:06 -0300

    fragment , container

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import f7 from './assets/f7.jpg';
 import ManageData from './components/ManageData';
@@ -7,9 +7,11 @@ import ConditionalRender from './components/ConditionalRender';
 import ShowUserName from './components/ShowUserName';
 import { useState } from 'react';
 import CarDetails from './components/CarDetails';
+import Fragment from './components/Fragment';
+import Container from './components/Container';
 
 function App() {
-  const name ="Pedrin"
+  //const name ="Pedrin"
   const [userName] = useState("Marquin")
 
   const cars = [
@@ -42,6 +44,15 @@ function App() {
       {cars.map((car) =>(
         <CarDetails  brand ={car.brand} color={car.color} km={car.km} newCar={car.newCar}/>
       ))}
+      {/*fragment */}
+      <Fragment propFragment ="teste"/>
+      {/*children*/}
+      <Container myValue="testing">
+        <p>e este é o conteudo</p>
+      </Container>
+      <Container myValue="testing 2">
+        <h4>Testando o container</h4>
+      </Container>
     </div>
   );
 }
