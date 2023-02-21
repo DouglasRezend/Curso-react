d707dafab335fbcfd4af7e9a51148997b3010472
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-02-21 11:22:01 -0300

    .

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -4,8 +4,13 @@ import f7 from './assets/f7.jpg';
 import ManageData from './components/ManageData';
 import ListRender from './components/ListRender';
 import ConditionalRender from './components/ConditionalRender';
+import ShowUserName from './components/ShowUserName';
+import { useState } from 'react';
+import CarDetails from './components/CarDetails';
 
 function App() {
+  const name ="Pedrin"
+  const [userName] = useState("marquin")
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -20,6 +25,13 @@ function App() {
       <ManageData/>
       <ListRender/>
       <ConditionalRender/>
+      {/*props*/}
+      <ShowUserName name={userName}/>
+      {/*destructuring*/}
+      <CarDetails brand="Vw" km={100000} color="Azul" newCar ={false}/>
+      {/*reaproveitamento*/}
+      <CarDetails brand ="Ford" km={0} color="Vermelha" newCar ={true} />
+      <CarDetails brand ="Fiat" km={1000} color="Preto" newCar ={false} />
     </div>
   );
 }
