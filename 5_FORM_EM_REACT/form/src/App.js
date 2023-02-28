eaad84444536e5acf026b8c4dbecc49161f959ea
Douglas  Rezende <117446649+DouglasRezend@users.noreply.github.com>
2023-02-28 12:23:43 -0300

    forms

--- 5_FORM_EM_REACT/form/src/App.js
+++ 5_FORM_EM_REACT/form/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import MyForm from './components/MyForm';
 
 function App() {
   return (
     <div className="App">
       <h2>Forms</h2>
-      
+      <MyForm/>
     
     </div>
   );
